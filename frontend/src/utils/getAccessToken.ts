50554d346600fd49bfcf04dea43cecc4203879c7
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2025-01-06 02:14:17 +0530

    profile page added

--- frontend/src/utils/getAccessToken.ts
+++ frontend/src/utils/getAccessToken.ts
@@ -1,11 +1,23 @@
 export function getAccessToken(): string {
+	const storedUser = localStorage.getItem("user");
+	let token = "";
+	if (storedUser) {
+		token = JSON.parse(storedUser).accessToken;
+	} else {
+		console.log("NOT TOKEN FOUND");
+		return "";
+	}
+	return `Bearer ${token}`;
+}
+
+export function getUserRole(): string{
     const storedUser = localStorage.getItem("user");
-    let token=""
+    let role = "";
     if (storedUser) {
-         token = JSON.parse(storedUser).accessToken;
-    }else{
-        console.log("NOT TOKEN FOUND")
+        role = JSON.parse(storedUser).role;
+    } else {
+        console.log("NOT USER FOUND");
         return "";
     }
-    return `Bearer ${token}`;
+    return role;
 }
